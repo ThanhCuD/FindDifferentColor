7a1b0be76e14b7f538fafdc2d87dcf50a6f29e31
ThanhCuD <thanhduc552018@gmail.com>
2020-05-07 17:05:31 +0700

    add auto

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.txtPoint = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,10 +39,11 @@
             // 
             this.button1.Location = new System.Drawing.Point(12, 29);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(163, 23);
+            this.button1.Size = new System.Drawing.Size(163, 116);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
             // 
             // pictureBox1
@@ -54,21 +56,31 @@
             // 
             // txtPoint
             // 
-            this.txtPoint.Location = new System.Drawing.Point(12, 86);
+            this.txtPoint.Location = new System.Drawing.Point(12, 325);
             this.txtPoint.Name = "txtPoint";
             this.txtPoint.Size = new System.Drawing.Size(163, 22);
             this.txtPoint.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(816, 569);
+            this.ClientSize = new System.Drawing.Size(815, 572);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtPoint);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -80,6 +92,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox txtPoint;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -20,72 +21,13 @@ namespace WindowsFormsApp2
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void button1_KeyDown(object sender, KeyEventArgs args)
         {
-            if (args.KeyCode == Keys.Space)
-            {
-                try
-                {
-                    var arr = getPoint();
-                    num++;
-                    //if (num < 4)
-                    //{
-                    //    arr = new List<int> { 150, 280 };
-                    //}
-                    //else if (num < 10)
-                    //{
-                    //    arr = new List<int> { 100, 200, 300 };
-                    //}
-                    //else if (num < 20)
-                    //{
-                    //    arr = new List<int> { 50, 150, 250, 350 };
-                    //}
-                    //else if (num < 30)
-                    //{
-                        arr = new List<int> { 50, 125, 200, 275, 350 };
-                    //}
-                    CaptureScreen();
-                    var bmp = (Bitmap)Image.FromFile(@"teszt" + num + ".jpg");
-                    Color pixelColor = bmp.GetPixel(0, 0);
-                    Dictionary<int[], Color> dic = new Dictionary<int[], Color>();
-                    foreach (var x in arr)
-                    {
-                        foreach (var y in arr)
-                        {
-                            var ar = new int[] { x, y };
-                            dic.Add(ar, bmp.GetPixel(x, y));
-                        }
-                    }
-                    var dicDistin = dic.Values.Distinct();
-                    foreach (var item in dicDistin)
-                    {
-                        if (dic.Values.Where(_ => _ == item).Count() == 1)
-                        {
-                            var find = dic.FirstOrDefault(_ => _.Value == item);
-                            for (int i = find.Key[0]; i < find.Key[0] + 10; i++)
-                            {
-                                for (int j = find.Key[1]; j < find.Key[1] + 10; j++)
-                                {
-                                    bmp.SetPixel(i, j, Color.White);
-                                }
-                            }
-                        }
-                    }
-
-                    pictureBox1.Image = null;
-                    bmp.Save("recolor" + num + ".png", ImageFormat.Png);
-                    pictureBox1.Image = Image.FromFile("recolor" + num + ".png");
-                    pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
-                }
-                catch
-                {
-
-                }
-
-            }
-
+            var curent = System.Windows.Forms.Cursor.Position;
+            label1.Text = curent.X + ", " + curent.Y;
         }
 
         public void CaptureScreen()
@@ -96,6 +38,7 @@ namespace WindowsFormsApp2
                 //Creating a New Graphics Object
                 Graphics captureGraphics = Graphics.FromImage(captureBitmap);
                 //Copying Image from The Screen
+
                 captureGraphics.CopyFromScreen(750, 480, 0, 0, Screen.PrimaryScreen.Bounds.Size,
                             CopyPixelOperation.SourceCopy);
                 //Saving the Image File (I am here Saving it in My E drive).
@@ -106,17 +49,87 @@ namespace WindowsFormsApp2
                 MessageBox.Show(ex.Message);
             }
         }
-        private List<int> getPoint()
+
+        private void button1_Click(object sender, EventArgs e)
         {
+            num++;
             try
             {
-                var arr = txtPoint.Text.Split(',').Select(_ => int.Parse(_)).ToList();
-                return arr;
+                var arr = new List<int>();
+                if (num < 4)
+                {
+                    arr = new List<int> { 150, 280 };
+                }
+                else if (num < 10)
+                {
+                    arr = new List<int> { 100, 200, 300 };
+                }
+                else if (num < 20)
+                {
+                    arr = new List<int> { 50, 150, 250, 350 };
+                }
+                else if (num < 30)
+                {
+                    arr = new List<int> { 50, 125, 200, 275, 350 };
+                }
+                CaptureScreen();
+                var bmp = (Bitmap)Image.FromFile(@"teszt" + num + ".jpg");
+                Color pixelColor = bmp.GetPixel(0, 0);
+                Dictionary<int[], Color> dic = new Dictionary<int[], Color>();
+                foreach (var x in arr)
+                {
+                    foreach (var y in arr)
+                    {
+                        var ar = new int[] { x, y };
+                        dic.Add(ar, bmp.GetPixel(x, y));
+                    }
+                }
+                var dicDistin = dic.Values.Distinct();
+                foreach (var item in dicDistin)
+                {
+                    if (dic.Values.Where(_ => _ == item).Count() == 1)
+                    {
+                        var find = dic.FirstOrDefault(_ => _.Value == item);
+                        MouseOperations.SetCursorPosition(720, 502);
+                        MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+                        MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
+                        Point location = button1.PointToScreen(Point.Empty);
+
+                        Process p = Process.GetCurrentProcess();
+                        string name = p.ProcessName;
+                        IntPtr hWnd = MouseOperations.FindWindow("From1", null);
+                        if (hWnd != IntPtr.Zero)
+                        {
+                            MouseOperations.ShowWindow(hWnd, 500);
+                            MouseOperations.SetForegroundWindow(hWnd);
+                        }
+
+                            MouseOperations.SetCursorPosition(location.X+10, location.Y+20);
+                        for (int i = find.Key[0]; i < find.Key[0] + 10; i++)
+                        {
+                            for (int j = find.Key[1]; j < find.Key[1] + 10; j++)
+                            {
+                                bmp.SetPixel(i, j, Color.White);
+                                
+                            }
+                        }
+                    }
+                }
+
+                pictureBox1.Image = null;
+                bmp.Save("recolor" + num + ".png", ImageFormat.Png);
+                pictureBox1.Image = Image.FromFile("recolor" + num + ".png");
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
             }
             catch
             {
-                return new List<int>();
+
             }
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            this.Activate();
         }
     }
 }
