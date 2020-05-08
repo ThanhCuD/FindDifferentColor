3948516001642ed9add15196c2fdd8968521375d
ThanhCuD <thanhduc552018@gmail.com>
2020-05-08 23:44:09 +0700

    new

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,59 +29,77 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.txtPoint = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.btResetLevel = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 29);
+            this.button1.Location = new System.Drawing.Point(9, 24);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(163, 116);
+            this.button1.Size = new System.Drawing.Size(122, 94);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Click";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(193, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(611, 545);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
             // txtPoint
             // 
-            this.txtPoint.Location = new System.Drawing.Point(12, 325);
+            this.txtPoint.Location = new System.Drawing.Point(9, 264);
+            this.txtPoint.Margin = new System.Windows.Forms.Padding(2);
             this.txtPoint.Name = "txtPoint";
-            this.txtPoint.Size = new System.Drawing.Size(163, 22);
+            this.txtPoint.Size = new System.Drawing.Size(123, 20);
             this.txtPoint.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(61, 148);
+            this.label1.Location = new System.Drawing.Point(28, 136);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 3;
+            // 
+            // btResetLevel
+            // 
+            this.btResetLevel.Location = new System.Drawing.Point(9, 166);
+            this.btResetLevel.Margin = new System.Windows.Forms.Padding(2);
+            this.btResetLevel.Name = "btResetLevel";
+            this.btResetLevel.Size = new System.Drawing.Size(122, 31);
+            this.btResetLevel.TabIndex = 4;
+            this.btResetLevel.Text = "Reset Level";
+            this.btResetLevel.UseVisualStyleBackColor = true;
+            this.btResetLevel.Click += new System.EventHandler(this.btResetLevel_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 214);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "N";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(815, 572);
+            this.ClientSize = new System.Drawing.Size(142, 298);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btResetLevel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtPoint);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,9 +108,10 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox txtPoint;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btResetLevel;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,17 +17,106 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
-        static int num = 0;
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+        static int num = 1;
+        // this make window can alway on top
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private const UInt32 SWP_NOSIZE = 0x0001;
+        private const UInt32 SWP_NOMOVE = 0x0002;
+        private const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+        //
         public Form1()
         {
             InitializeComponent();
-
         }
+        Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+        public Color GetColorAt(Point location)
+        {
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr hSrcDC = gsrc.GetHdc();
+                    IntPtr hDC = gdest.GetHdc();
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
 
+            return screenPixel.GetPixel(0, 0);
+        }
         private void button1_KeyDown(object sender, KeyEventArgs args)
         {
             var curent = System.Windows.Forms.Cursor.Position;
             label1.Text = curent.X + ", " + curent.Y;
+            num++;
+            label2.Text = num.ToString();
+            try
+            {
+
+                var arrX = new List<int>();
+                var arrY = new List<int>();
+                if (num <= 4)
+                {
+                    arrX = new List<int> { 636, 728 };
+                    arrY = new List<int> { 448, 540 };
+                }
+                else if (num <= 10)
+                {
+                    arrX = new List<int> { 591, 687, 767 };
+                    arrY = new List<int> { 400, 473, 557 };
+                }
+                else if (num <= 20)
+                {
+                    arrX = new List<int> { 579, 650, 721, 782 };
+                    arrY = new List<int> { 386, 459, 529, 549 };
+                }
+                else if (num <= 30)
+                {
+                    arrX = new List<int> { 579, 650, 721, 782 };
+                    arrY = new List<int> { 386, 459, 529, 549 };
+                }
+
+
+                Dictionary<int[], Color> dic = new Dictionary<int[], Color>();
+                foreach (var x in arrX)
+                {
+                    foreach (var y in arrY)
+                    {
+                        var point = new Point(x, y);
+                        var color = GetColorAt(point);
+                        var ar = new int[] { x, y };
+                        dic.Add(ar, color);
+                    }
+                }
+                var dicDistin = dic.Values.Distinct();
+                foreach (var item in dicDistin)
+                {
+                    if (dic.Values.Where(_ => _ == item).Count() == 1)
+                    {
+                        var find = dic.FirstOrDefault(_ => _.Value == item);
+                        MouseOperations.SetCursorPosition(find.Key[0], find.Key[1]);
+                        MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+                        MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
+                        Point location = button1.PointToScreen(Point.Empty);
+                        //MouseOperations.SetCursorPosition(location.X + 10, location.Y + 10);
+                    }
+                }
+            }
+            catch
+            {
+
+            }
         }
 
         public void CaptureScreen()
@@ -52,79 +141,16 @@ namespace WindowsFormsApp2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            num++;
-            try
-            {
-                var arr = new List<int>();
-                if (num < 4)
-                {
-                    arr = new List<int> { 150, 280 };
-                }
-                else if (num < 10)
-                {
-                    arr = new List<int> { 100, 200, 300 };
-                }
-                else if (num < 20)
-                {
-                    arr = new List<int> { 50, 150, 250, 350 };
-                }
-                else if (num < 30)
-                {
-                    arr = new List<int> { 50, 125, 200, 275, 350 };
-                }
-                CaptureScreen();
-                var bmp = (Bitmap)Image.FromFile(@"teszt" + num + ".jpg");
-                Color pixelColor = bmp.GetPixel(0, 0);
-                Dictionary<int[], Color> dic = new Dictionary<int[], Color>();
-                foreach (var x in arr)
-                {
-                    foreach (var y in arr)
-                    {
-                        var ar = new int[] { x, y };
-                        dic.Add(ar, bmp.GetPixel(x, y));
-                    }
-                }
-                var dicDistin = dic.Values.Distinct();
-                foreach (var item in dicDistin)
-                {
-                    if (dic.Values.Where(_ => _ == item).Count() == 1)
-                    {
-                        var find = dic.FirstOrDefault(_ => _.Value == item);
-                        MouseOperations.SetCursorPosition(720, 502);
-                        MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
-                        MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
-                        Point location = button1.PointToScreen(Point.Empty);
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+        }
 
-                        Process p = Process.GetCurrentProcess();
-                        string name = p.ProcessName;
-                        IntPtr hWnd = MouseOperations.FindWindow("From1", null);
-                        if (hWnd != IntPtr.Zero)
-                        {
-                            MouseOperations.ShowWindow(hWnd, 500);
-                            MouseOperations.SetForegroundWindow(hWnd);
-                        }
-
-                            MouseOperations.SetCursorPosition(location.X+10, location.Y+20);
-                        for (int i = find.Key[0]; i < find.Key[0] + 10; i++)
-                        {
-                            for (int j = find.Key[1]; j < find.Key[1] + 10; j++)
-                            {
-                                bmp.SetPixel(i, j, Color.White);
-                                
-                            }
-                        }
-                    }
-                }
-
-                pictureBox1.Image = null;
-                bmp.Save("recolor" + num + ".png", ImageFormat.Png);
-                pictureBox1.Image = Image.FromFile("recolor" + num + ".png");
-                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
-            }
-            catch
-            {
-
-            }
+        private void btResetLevel_Click(object sender, EventArgs e)
+        {
+            num = 1;
         }
 
         private void Form1_Deactivate(object sender, EventArgs e)
